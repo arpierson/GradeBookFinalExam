8a727b7e4069a2a37f6ece1754226f278ec9eb8c
Allen Pierson <apierso2@my.westga.edu>
2019-04-20 19:59:24 -0400

    - Created StudentGradeBook superclass - Wrote constructor, addGrade, and getter for pointsEarnedList

---
+++ src/edu/westga/cs6312/gradebook/model/StudentGradeBook.java
@@ -0,0 +1,42 @@
+package edu.westga.cs6312.gradebook.model;
+
+import java.util.ArrayList;
+
+/**
+ * Abstract superclass for GradeBook type object
+ * @author Allen Pierson
+ * @version 041202019
+ *
+ */
+public abstract class StudentGradeBook {
+	private ArrayList<Double> pointsEarnedList;
+	
+	/**
+	 * 0-parameter constructor to instantiate the instance variable
+	 */
+	public StudentGradeBook() {
+		this.pointsEarnedList = new ArrayList<Double>();
+	}
+	
+	/**
+	 * Method to add a grade to the ArrayList
+	 * @param pointsEarned	points earned on the assignment
+	 * 
+	 * Precondition:	pointsEarned !< 0
+	 * 					pointsPossible !< 0
+	 */
+	public void addGrade(double pointsEarned) {
+		if (pointsEarned <= -1) {
+			throw new IllegalArgumentException("Points earned cannot be less than 0");
+		}
+		this.pointsEarnedList.add(pointsEarned);
+	}
+	
+	/**
+	 * Getter method for the student grade list
+	 * @return the grade list
+	 */
+	public ArrayList<Double> getPointsEarnedList() {
+		return this.pointsEarnedList;
+	}
+}
