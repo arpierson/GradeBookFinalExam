698fba9556771592c1f003938f5e0b2a55beaeeb
Allen Pierson <apierso2@my.westga.edu>
2019-04-21 19:30:19 -0400

    - Wrote JUnit tests for ClassroomData.getStudentProjectAverage

---
+++ src/edu/westga/cs6312/gradebook/testing/ClassroomDataWhenGetStudentProjectAverage.java
@@ -0,0 +1,51 @@
+package edu.westga.cs6312.gradebook.testing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6312.gradebook.model.ClassroomData;
+import edu.westga.cs6312.gradebook.model.Student;
+
+/**
+ * 
+ * @author Allen Pierson
+ * @version 04212019
+ *
+ */
+class ClassroomDataWhenGetStudentProjectAverage {
+
+	@Test
+	void testWithThreeProjectGrades() {
+		ClassroomData theClassroom = new ClassroomData();
+		theClassroom.addStudent(1234, "Allen", "Pierson");
+		Student theStudent = theClassroom.getStudent(1234);
+		theClassroom.addAssignment(1,  10);
+		theClassroom.addAssignment(1,  10);
+		theClassroom.addAssignment(1,  10);
+		theStudent.addGrade(1, 9);
+		theStudent.addGrade(1,  8.5);
+		theStudent.addGrade(1,  7);
+		double result = theClassroom.getStudentProjectAverage(theStudent);
+		assertEquals(81.7, result);
+	}
+
+	@Test
+	void testWithFiveProjectGrades() {
+		ClassroomData theClassroom = new ClassroomData();
+		theClassroom.addStudent(1234, "Allen", "Pierson");
+		Student theStudent = theClassroom.getStudent(1234);
+		theClassroom.addAssignment(1,  10);
+		theClassroom.addAssignment(1,  10);
+		theClassroom.addAssignment(1,  10);
+		theClassroom.addAssignment(1,  10);
+		theClassroom.addAssignment(1,  10);
+		theStudent.addGrade(1, 9);
+		theStudent.addGrade(1,  8.5);
+		theStudent.addGrade(1,  7);
+		theStudent.addGrade(1,  10);
+		theStudent.addGrade(1,  0);
+		double result = theClassroom.getStudentProjectAverage(theStudent);
+		assertEquals(69, result);
+	}
+}
