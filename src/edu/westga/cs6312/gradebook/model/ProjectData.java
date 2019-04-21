8275c2901e21699e6247f757e8e53ea6d2031410
Allen Pierson <apierso2@my.westga.edu>
2019-04-21 15:30:29 -0400

    - Created ProjectData child class of StudentGradeBook

---
+++ src/edu/westga/cs6312/gradebook/model/ProjectData.java
@@ -0,0 +1,17 @@
+package edu.westga.cs6312.gradebook.model;
+
+/**
+ * Creates a Lab based off of a StudentGradeBook to track all lab grades for a student
+ * @author Allen Pierson
+ * @version 04212019
+ *
+ */
+public class ProjectData extends StudentGradeBook {
+
+	/**
+	 * Constructor that creates a ProjectData object
+	 */
+	public ProjectData() {
+		super();
+	}
+}
