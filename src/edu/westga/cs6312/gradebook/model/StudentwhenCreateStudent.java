bf0f653ecc0b8b8672ba43d781caad8e03d44dc1
Allen Pierson <apierso2@my.westga.edu>
2019-04-21 15:52:23 -0400

    - Wrote JUnit tests for Student constructor and toString

---
+++ src/edu/westga/cs6312/gradebook/model/StudentwhenCreateStudent.java
@@ -0,0 +1,29 @@
+package edu.westga.cs6312.gradebook.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ * @author Allen Pierson
+ * @version 04212019
+ *
+ */
+class StudentwhenCreateStudent {
+
+	@Test
+	void testWhenStudentIs1234AllenPierson() {
+		Student testStudent = new Student(1234, "Allen", "Pierson");
+		String result = testStudent.toString();
+		assertEquals("1234 Allen Pierson", result);
+	}
+	
+	@Test
+	void testWhenStudentis4321MauriceMoss() {
+		Student testStudent = new Student(4321, "Maurice", "Moss");
+		String result = testStudent.toString();
+		assertEquals("4321 Maurice Moss", result);
+	}
+
+}
