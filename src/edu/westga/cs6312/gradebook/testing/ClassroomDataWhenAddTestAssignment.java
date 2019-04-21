ad51eff77fa7d0e0a7a876f94e6b86b0df98455e
Allen Pierson <apierso2@my.westga.edu>
2019-04-21 16:43:44 -0400

    - Wrote JUnit tests for ClassroomData.getTestAssignmentList

---
+++ src/edu/westga/cs6312/gradebook/testing/ClassroomDataWhenAddTestAssignment.java
@@ -0,0 +1,34 @@
+package edu.westga.cs6312.gradebook.testing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6312.gradebook.model.ClassroomData;
+
+/**
+ * 
+ * @author Allen Pierson
+ * @version 04212019
+ *
+ */
+class ClassroomDataWhenAddTestAssignment {
+
+	@Test
+	void testWhenAdding1TestGrade() {
+		ClassroomData testClassroom = new ClassroomData();
+		testClassroom.addAssingment(2, 10);
+		double result = testClassroom.getTestAssignmentList().get(0);
+		assertEquals(10, result);
+	}
+
+	@Test
+	void testWhenAdding3TestGrades() {
+		ClassroomData testClassroom = new ClassroomData();
+		testClassroom.addAssingment(2, 10);
+		testClassroom.addAssingment(2, 20);
+		testClassroom.addAssingment(2, 30);
+		double result = testClassroom.getTestAssignmentList().get(2);
+		assertEquals(30, result);
+	}
+}
