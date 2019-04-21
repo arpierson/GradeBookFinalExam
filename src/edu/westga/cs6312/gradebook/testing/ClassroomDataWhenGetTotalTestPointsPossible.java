eabac4ba94290560cfb99a63157ce0292e43eea4
Allen Pierson <apierso2@my.westga.edu>
2019-04-21 16:59:04 -0400

    - Wrote JUnit tests for ClassroomDataWhenGetTotalTestPointsPossible

---
+++ src/edu/westga/cs6312/gradebook/testing/ClassroomDataWhenGetTotalTestPointsPossible.java
@@ -0,0 +1,43 @@
+package edu.westga.cs6312.gradebook.testing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6312.gradebook.model.ClassroomData;
+
+/**
+ * 
+ * @author Allen Pierson
+ * @version 04212019
+ *
+ */
+class ClassroomDataWhenGetTotalTestPointsPossible {
+
+	@Test
+	void testWith3TestGrades() {
+		ClassroomData testClassroom = new ClassroomData();
+		testClassroom.addAssingment(2, 10);
+		testClassroom.addAssingment(2, 10);
+		testClassroom.addAssingment(2, 10);
+		double result = testClassroom.getTotalTestPointsPossible();
+		assertEquals(30, result);
+	}
+
+	@Test
+	void testWith10TestGrades() {
+		ClassroomData testClassroom = new ClassroomData();
+		testClassroom.addAssingment(2, 10);
+		testClassroom.addAssingment(2, 10);
+		testClassroom.addAssingment(2, 10);
+		testClassroom.addAssingment(2, 10);
+		testClassroom.addAssingment(2, 10);
+		testClassroom.addAssingment(2, 10);
+		testClassroom.addAssingment(2, 10);
+		testClassroom.addAssingment(2, 10);
+		testClassroom.addAssingment(2, 10);
+		testClassroom.addAssingment(2, 10);
+		double result = testClassroom.getTotalTestPointsPossible();
+		assertEquals(100, result);
+	}
+}
