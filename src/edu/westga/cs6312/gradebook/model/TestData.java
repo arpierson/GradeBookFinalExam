c006b3116339140731f90bc8a6b6036ccad73c47
Allen Pierson <apierso2@my.westga.edu>
2019-04-21 15:34:28 -0400

    - Created TestData child class of StudentGradeBook

---
+++ src/edu/westga/cs6312/gradebook/model/TestData.java
@@ -0,0 +1,17 @@
+package edu.westga.cs6312.gradebook.model;
+
+/**
+ * Creates a Lab based off of a StudentGradeBook to track all lab grades for a student
+ * @author Allen Pierson
+ * @version 04212019
+ *
+ */
+public class TestData extends StudentGradeBook {
+
+	/**
+	 * Constructor that creates a TestData object
+	 */
+	public TestData() {
+		super();
+	}
+}
