4ec3289a24f9f2de5d00a11d2f43211e2f980722
Allen Pierson <apierso2@my.westga.edu>
2019-04-21 18:43:37 -0400

    - Wrote JUnit tests for ClassroomData.addStudent and getStudent

--- src/edu/westga/cs6312/gradebook/testing/ClassroomDataWhenAddStudent.java
+++ src/edu/westga/cs6312/gradebook/testing/ClassroomDataWhenAddStudent.java
@@ -1,8 +1,10 @@
-package edu.westga.cs6312.gradebook.model;
+package edu.westga.cs6312.gradebook.testing;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6312.gradebook.model.ClassroomData;
 
 class ClassroomDataWhenAddStudent {
 
@@ -10,15 +12,17 @@ class ClassroomDataWhenAddStudent {
 	void testWhenAdd1234AllenPierson() {
 		ClassroomData theClassroom = new ClassroomData();
 		theClassroom.addStudent(1234, "Allen", "Pierson");
-		String result = theClassroom.getStudentList().get(0).toString();
+		theClassroom.addStudent(4321, "Maurice", "Moss");
+		String result = theClassroom.getStudent(1234).toString();
 		assertEquals("1234 Allen Pierson", result);
 	}
 
 	@Test
 	void testWhenStudentis4321MauriceMoss() {
 		ClassroomData theClassroom = new ClassroomData();
+		theClassroom.addStudent(1234, "Allen", "Pierson");
 		theClassroom.addStudent(4321, "Maurice", "Moss");
-		String result = theClassroom.getStudentList().get(0).toString();
+		String result = theClassroom.getStudent(4321).toString();
 		assertEquals("4321 Maurice Moss", result);
 	}
 }
