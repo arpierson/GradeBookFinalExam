0b69c72be955664fc43776953de3f518569efa36
Allen Pierson <apierso2@my.westga.edu>
2019-04-20 20:01:33 -0400

    - Created LabData class from StudentGradeBook superclass - Wrote constructor

---
+++ src/edu/westga/cs6312/gradebook/model/LabData.java
@@ -0,0 +1,17 @@
+package edu.westga.cs6312.gradebook.model;
+
+/**
+ * Creates a Lab based off of a StudentGradeBook to track all lab grades for a student
+ * @author Allen Pierson
+ * @version 04202019
+ *
+ */
+public class LabData extends StudentGradeBook {
+
+	/**
+	 * Constructor that creates a LabData object
+	 */
+	public LabData() {
+		super();
+	}
+}
