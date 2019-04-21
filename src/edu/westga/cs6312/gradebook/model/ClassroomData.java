3e68014a0efd0255a82ae0cfa5b31956ad177b2e
Allen Pierson <apierso2@my.westga.edu>
2019-04-21 16:20:28 -0400

    - Wrote ClassroomData.addAssignment - Added instance variables to ClassroomData

--- src/edu/westga/cs6312/gradebook/model/ClassroomData.java
+++ src/edu/westga/cs6312/gradebook/model/ClassroomData.java
@@ -10,13 +10,40 @@ import java.util.ArrayList;
  */
 public class ClassroomData {
 	private ArrayList<Student> studentList;
-	private ArrayList<Double> assignmentList;
+	private ArrayList<Double> labAssignmentList;
+	private ArrayList<Double> projectAssignmentList;
+	private ArrayList<Double> testAssignmentList;
 	
 	/**
 	 * 0-parameter constructor to instantiate the instance variables
 	 */
 	public ClassroomData() {
 		this.studentList = new ArrayList<Student>();
-		this.assignmentList = new ArrayList<Double>();
+		this.labAssignmentList = new ArrayList<Double>();
+		this.projectAssignmentList = new ArrayList<Double>();
+		this.testAssignmentList = new ArrayList<Double>();
+	}
+	
+	/**
+	 * Adds a new assignment to the lab, project, or test list based on gradeTypeChoice
+	 * @param gradeTypeChoice	choice of which grade type (lab, project, or test)
+	 * @param pointsPossible	points possible on the assignment
+	 * 
+	 * Precondition:	gradeTypeChoice must be 1, 2, or 3
+	 * 					pointsPossible !< 0
+	 */
+	public void addAssingment(int gradeTypeChoice, double pointsPossible) {
+		if (pointsPossible <= -1) {
+			throw new IllegalArgumentException("Assignment cannot be worth less than 0");
+		}
+		switch (gradeTypeChoice) {
+			case 1: this.labAssignmentList.add(pointsPossible);
+					break;
+			case 2: this.projectAssignmentList.add(pointsPossible);
+					break;
+			case 3: this.testAssignmentList.add(pointsPossible);
+					break;
+			default: throw new IllegalArgumentException("Grade type (lab, project, or test) not specified");
+		}
 	}
 }
