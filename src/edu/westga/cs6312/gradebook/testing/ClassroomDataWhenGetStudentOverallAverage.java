41924600d313f0c3c4eb516e1c6b9655dffd0289
Allen Pierson <apierso2@my.westga.edu>
2019-04-23 19:18:35 -0400

    - Shortened test to comply with CheckStyle rules

--- src/edu/westga/cs6312/gradebook/testing/ClassroomDataWhenGetStudentOverallAverage.java
+++ src/edu/westga/cs6312/gradebook/testing/ClassroomDataWhenGetStudentOverallAverage.java
@@ -43,41 +43,33 @@ class ClassroomDataWhenGetStudentOverallAverage {
 	}
 
 	@Test
-	void testWithFiveGradesOfEachType() {
+	void testWithThreeLabsAndFourTestsAndProjects() {
 		ClassroomData theClassroom = new ClassroomData();
-		theClassroom.addAssignment(0, 5);
 		theClassroom.addAssignment(0, 10);
 		theClassroom.addAssignment(0, 10);
 		theClassroom.addAssignment(0, 10);
-		theClassroom.addAssignment(0, 20);
 		theClassroom.addAssignment(1, 50);
 		theClassroom.addAssignment(1, 50);
 		theClassroom.addAssignment(1, 100);
 		theClassroom.addAssignment(1, 100);
-		theClassroom.addAssignment(1, 100);
-		theClassroom.addAssignment(2, 100);
 		theClassroom.addAssignment(2, 100);
 		theClassroom.addAssignment(2, 100);
 		theClassroom.addAssignment(2, 100);
 		theClassroom.addAssignment(2, 100);
 		theClassroom.addStudent(1234, "Allen", "Pierson");
 		Student theStudent = theClassroom.getStudent(1234);
-		theStudent.addGrade(0, 5);
 		theStudent.addGrade(0, 10);
 		theStudent.addGrade(0, 8);
 		theStudent.addGrade(0, 8);
-		theStudent.addGrade(0, 17.5);
 		theStudent.addGrade(1, 45);
 		theStudent.addGrade(1, 48.9);
 		theStudent.addGrade(1, 90);
 		theStudent.addGrade(1, 88.5);
-		theStudent.addGrade(1, 72.75);
 		theStudent.addGrade(2, 100);
 		theStudent.addGrade(2, 0);
 		theStudent.addGrade(2, 95);
 		theStudent.addGrade(2, 100);
-		theStudent.addGrade(2, 86.2);
 		double result = theClassroom.getStudentOverallAverage(theStudent);
-		assertEquals(81.1, result);
+		assertEquals(81.3, result);
 	}
 }
