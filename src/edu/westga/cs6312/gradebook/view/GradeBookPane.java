cd830533c857b1f73f1c797e77c549ff03d3ec3b
Allen Pierson <apierso2@my.westga.edu>
2019-04-23 16:13:04 -0400

    - Wrote GradeBookPane constructor, .setPaneSize, .setPaneLayout, and .setMenuBar

--- src/edu/westga/cs6312/gradebook/view/GradeBookPane.java
+++ src/edu/westga/cs6312/gradebook/view/GradeBookPane.java
@@ -1,6 +1,12 @@
 package edu.westga.cs6312.gradebook.view;
 
+import java.io.File;
+
 import edu.westga.cs6312.gradebook.model.ClassroomData;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
 /**
@@ -17,5 +23,36 @@ public class GradeBookPane extends Pane {
 	 */
 	public GradeBookPane() {
 		this.theClassroom = new ClassroomData();
+	}
+	
+	private void setPaneSize() {
+		this.setPrefSize(500, 600);
+	}
+	
+	private void setPaneLayout() {
+		BorderPane mainOverlay = new BorderPane();
+		mainOverlay.prefWidthProperty().bind(this.widthProperty());
+		mainOverlay.prefHeightProperty().bind(this.heightProperty());
+		mainOverlay.setTop(this.setMenuBar());
+		
+		this.getChildren().add(mainOverlay);
+	}
+	
+	private MenuBar setMenuBar() {
+		MenuBar theMenuBar = new MenuBar();
+		Menu file = new Menu("File");
+		MenuItem open = new MenuItem("Open");
+		open.setOnAction(openFile -> {
+			File dataFile = this.fileReader.chooseFile();
+			this.fileReader.setAssignmentPointsPossible(dataFile);
+		});
+		file.getItems().add(open);
+		
+		Menu about = new Menu("About");
+		MenuItem help = new MenuItem("Help");
+		about.getItems().add(help);
+		
+		theMenuBar.getMenus().addAll(file, about);
+		return theMenuBar;
 	}
 }
