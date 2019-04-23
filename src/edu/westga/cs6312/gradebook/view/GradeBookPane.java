211819784a754525a1d392585353f9c2d5b2e51d
Allen Pierson <apierso2@my.westga.edu>
2019-04-23 16:26:49 -0400

    - Wrote GradeBookPane inner class FileReader and methods

--- src/edu/westga/cs6312/gradebook/view/GradeBookPane.java
+++ src/edu/westga/cs6312/gradebook/view/GradeBookPane.java
@@ -1,13 +1,19 @@
 package edu.westga.cs6312.gradebook.view;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 import edu.westga.cs6312.gradebook.model.ClassroomData;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 
 /**
  * Draws the GradeBook application GUI
@@ -17,12 +23,16 @@ import javafx.scene.layout.Pane;
  */
 public class GradeBookPane extends Pane {
 	private ClassroomData theClassroom;
+	private FileReader theFileReader;
 	
 	/**
 	 * 0-parameter constructor to instantiate the instance variable
 	 */
 	public GradeBookPane() {
 		this.theClassroom = new ClassroomData();
+		this.theFileReader = new FileReader();
+		this.setPaneSize();
+		this.setPaneLayout();
 	}
 	
 	private void setPaneSize() {
@@ -43,8 +53,8 @@ public class GradeBookPane extends Pane {
 		Menu file = new Menu("File");
 		MenuItem open = new MenuItem("Open");
 		open.setOnAction(openFile -> {
-			File dataFile = this.fileReader.chooseFile();
-			this.fileReader.setAssignmentPointsPossible(dataFile);
+			File dataFile = this.theFileReader.chooseFile();
+			this.theFileReader.readFile(dataFile);
 		});
 		file.getItems().add(open);
 		
@@ -54,5 +64,69 @@ public class GradeBookPane extends Pane {
 		
 		theMenuBar.getMenus().addAll(file, about);
 		return theMenuBar;
+	}
+	
+	class FileReader {
+		private FileChooser theFileChooser;
+		
+		FileReader() {
+			this.theFileChooser = new FileChooser();
+		}
+		
+		public File chooseFile() {
+			File selectedFile = this.theFileChooser.showOpenDialog(null);
+			if (selectedFile != null) {
+				return selectedFile;
+			} else {
+				return null;
+			}
+		}
+		
+		public void readFile(File theFile) {
+			try (Scanner inFile = new Scanner(theFile)) {
+				String[] classData = inFile.nextLine().split(",");
+				int assignmentType = 0;
+				for (int current = 3; current < classData.length; current++) {
+					if (classData[current].equals("")) {
+						assignmentType++;
+					} else {
+						GradeBookPane.this.theClassroom.addAssignment(assignmentType, Double.valueOf(classData[current]));
+					}
+				}
+				this.readStudentData(theFile);
+			} catch (FileNotFoundException fnfe) {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setContentText("The file could not be located or does not exist");
+				alert.showAndWait();
+			}
+		}
+		
+		private void readStudentData(File theFile) throws FileNotFoundException {
+			int gradeType = 0;
+			try (Scanner inFile = new Scanner(theFile)) {
+				while (inFile.hasNext()) {
+					inFile.nextLine();
+					try {
+						String[] studentData = inFile.nextLine().split(",");
+						GradeBookPane.this.theClassroom.addStudent(Integer.valueOf(studentData[0]), studentData[1], studentData[2]);
+						for (int current = 3; current < studentData.length; current++) {
+							if (studentData[current].equals("")) {
+								gradeType++;
+							} else {
+								GradeBookPane.this.theClassroom.getStudent(Integer.valueOf(studentData[0])).addGrade(gradeType, Double.valueOf(studentData[current]));
+							}
+						}
+					} catch (InputMismatchException | IndexOutOfBoundsException | IllegalArgumentException exception) {
+						Alert alert = new Alert(AlertType.ERROR);
+						alert.setContentText("Data Format Error at index ");
+						alert.showAndWait();
+					}
+				}
+			} catch (InputMismatchException | IndexOutOfBoundsException | IllegalArgumentException exception) {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setContentText("Data Format Error at index ");
+				alert.showAndWait();
+			}
+		}
 	}
 }
